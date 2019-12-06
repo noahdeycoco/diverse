f4f3bbd499b7ad3397c34a9f6e0362d57f681de0
noestl <31355872+noestl@users.noreply.github.com>
2019-12-06 19:08:57 +0100

    UPD

--- slashOS.py
+++ slashOS.py
@@ -2,15 +2,30 @@
 # coding = utf-8
 
 import os
+import shelve
 
-print(os.path.join('usr', 'home', 'spam'))
-print(os.getcwd())
+# print(os.path.join('usr', 'home', 'spam'))
+# print(os.getcwd())
 # os.chdir(os.path.join('..', '..', '..', 'Desktop'))
 
-try:
-    os.makedirs('test')
-except Exception:
-    Exception
+# try:
+#     os.makedirs('test')
+# except Exception:
+#     Exception
 
-os.path.realpath()
- 
+# os.path.realpath()
+
+# aa = open('test.txt', 'a')
+# aa.write('\n quatre')
+# aa.close()
+# content = open('test.txt')
+# print(content.read())
+
+shelfFile = shelve.open('mydata')
+cats = ['Zophie', 'Pooka', 'Simon']
+# shelfFile['cats'] = cats
+# shelfFile.close()
+
+print(shelfFile)
+print(shelfFile['cats'])
+print(list(shelfFile.values()))
