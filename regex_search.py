57e741675e8ae1c81c0a4cdb1bcbb09427b4690d
noestl <31355872+noestl@users.noreply.github.com>
2019-12-23 13:33:35 +0100

    ADD regex_search.py

--- regex_search.py
+++ regex_search.py
@@ -4,28 +4,52 @@ that matches a user-supplied regular expression. The results should be printed
 to the screen.
 """
 
-
 import random
-
-random_file_text = """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-"""
-
-
-import random
-import string
 import os
+import re
 
-initial_t = os.open(('data' + os.sep + 'intial.txt'), 'r')
-pritn(initial_t.readline())
+random_file_text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimveniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
 
-# print(random_file_text.split('\n'))
 
-# for i in range(4):
-#     random.shuffle(random_file_text.split('\n'))
-#     print(random_file_text[0:4])
+def write_initial_file():
+    try:
+        random_file_text_1 = open('data' + os.sep + 'initial.txt', 'w')
+        random_file_text_1.write(random_file_text)
+        random_file_text_1.close()
+        random_file_text_2 = open('data' + os.sep + 'initial.txt', 'r')
+        print(str(len(random_file_text_2.readlines())) 
+              + ' line(s) has been writen to initial.txt.')
+    except Exception:
+        print('An error occured: ' + str(Exception))
+
+
+def create_data(text):
+    try:
+        files_text_data = {}
+        for i in range(4):
+            files_text_data['text_data_%s' % i] = list()
+            for _ in range(15):
+                text_word = random.choice(text.split())
+                files_text_data['text_data_%s' % i].append(text_word)
+        return files_text_data
+    except Exception:
+        print('An error occured: ' + str(Exception))
+
+
+def write_files():
+    try:
+        for i in range(4):
+            random_file_text_1 = open('data' + os.sep + 'random_text_%s.txt' % i, 'w')
+            random_file_text_1.write(' '.join(create_data(random_file_text)['text_data_%s' % i]))
+            print(str(os.path.abspath('data' + os.sep + 'random_text_%s.txt' % i)) + ' has been created.')
+    except Exception:
+        print('An error occured: ' + str(Exception))
+
+def search_regex():
+    for _ range():
+
+
+if __name__ == '__main__':
+    write_initial_file()
+    create_data(random_file_text)
+    write_files()
