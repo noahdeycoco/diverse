aa7a2e762cdb7a45319aa92c583cc1c20a185da4
Noe <noesautel@icloud.com>
2020-05-08 18:14:15 +0200

    UPD mapit

--- mapit.py
+++ mapit.py
@@ -1,2 +1,15 @@
+# mapIt.py - Launches a map in the browser using an address from the
+# command line or clipboard.
+
 import webbrowser
-webbrowser.open('http://inventwithpython.com/')
+import sys
+
+if len(sys.argv) > 1:
+ # Get address from command line.
+ address = ' '.join(sys.argv[1:]).replace(' ','+')
+ 
+
+url = 'https://www.google.com/maps/place/'+address
+
+webbrowser.open(url)
+# print(address)
