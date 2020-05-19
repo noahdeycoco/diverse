75d6427facd8731a871937813f4ca20ec33590ab
Noe <noesautel@icloud.com>
2020-05-19 20:34:54 +0200

    ADD sendDuesReminders

--- sendDuesReminders.py
+++ sendDuesReminders.py
@@ -1,23 +1,16 @@
+import smtplib
+import json
 import pandas as pd
 
+
+# Read data from excel file
 df = pd.read_excel('data/duesRecords.xlsx')
 print(df)
 print('\n --- \n')
 
-df_jan14 = df.iloc[:, [0,1,2]]
-# print(df_jan14)
-
-# print(df_jan14.iloc[:, 2])
-
-# print(list(df.columns[2:]))
-
-
 df_columns_list_raw = list(df.columns[2:])
 
-df_columns_list =  []
-for i in df_columns_list_raw:
-  df_columns_list.append(i.replace(" ","_").lower())
-
+# Get people info regarding haven't paid
 unpaid_dict = {}
 for i in df_columns_list_raw:
   df_unpaid = df[['Member', 'Email', i]][df[i] != 'paid']
@@ -27,3 +20,29 @@ for i in df_columns_list_raw:
     unpaid_dict.update(df_unpaid.to_dict())
 
 # print(unpaid_dict)
+# print(unpaid_dict.keys())
+# print(list(unpaid_dict['Member'].keys()))
+# print(unpaid_dict['Member'][0])
+
+
+# Log in to an SMTP server by calling smtplib.SMTP(), ehlo(), starttls(),
+# and login().
+# For all members behind on their dues, send a personalized reminder
+# email by calling the sendmail() method.
+
+
+
+smtpObj = smtplib.SMTP('smtp.mail.me.com', 587)
+smtpObj.ehlo()
+smtpObj.starttls()
+
+with open('data/config.json') as json_file:
+    config = json.load(json_file)
+
+smtpObj.login(data.get('mail'), data.get('password'))
+
+# msg = ('From: {}\r\nTo: {}\r\n\r\nBonjour {} !'.format(smtpObj.user, smtpObj.user,'Noe'))
+
+# smtpObj.sendmail(from_addr=smtpObj.user, to_addrs=smtpObj.user, msg=msg)
+
+smtpObj.quit()
