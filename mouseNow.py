bf5bf5a7674131848f7e6ce60cf79f1dd1572724
Noe <noesautel@icloud.com>
2020-05-30 21:25:03 +0200

    UPD mousenow

--- mouseNow.py
+++ mouseNow.py
@@ -7,12 +7,23 @@ width, height = pyautogui.size()
 
 print(width, height)
 
-pyautogui.click(1767, 15)
+# pyautogui.click(1767, 15)
 
-try:
-  while True:
-    print(pyautogui.position(), '\b')
-    print('\b')
-    time.sleep(2)
-except KeyboardInterrupt:
-  print('Programm exit.')
+# try:
+#   while True:
+#     print(pyautogui.position(), '\b')
+#     print('\b')
+#     time.sleep(2)
+# except KeyboardInterrupt:
+#   print('Programm exit.')
+
+
+pyautogui.click() # click to put drawing program in focus
+distance = 200
+while distance > 0:
+  pyautogui.dragRel(distance, 0) # move right
+  distance = distance - 5
+  pyautogui.dragRel(0, distance,) # move down
+  pyautogui.dragRel(-distance, 0,) # move left
+  distance = distance - 5
+  pyautogui.dragRel(0, -distance) # move up
